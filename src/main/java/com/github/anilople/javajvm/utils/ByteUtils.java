d161f172c7cdac7c80e91b6cf5f3a2474d585ff1
Anilople <Anilople@outlook.com>
2019-10-30 20:00:43 +0800

    2 bytes to short, with testing

--- src/main/java/com/github/anilople/javajvm/utils/ByteUtils.java
+++ src/main/java/com/github/anilople/javajvm/utils/ByteUtils.java
@@ -35,4 +35,25 @@ public class ByteUtils {
             return 0;
         }
     }
+
+    /**
+     * convert 2 bytes to a short
+     * when a byte extend to an short
+     * 0xF0 -> 0xFFF0
+     * if you want 0xF0 -> 0x00F0
+     * you must use ((short) b & 0xFF) manually
+     * @param bytes length must be 2
+     * @return a short
+     */
+    public static short bytes2short(byte[] bytes) {
+        if(2 == bytes.length) {
+            int v0 = (bytes[0] & 0xFF) << 8;
+            int v1 = (bytes[1] & 0xFF) ;
+            short value =  (short) (v0 | v1);
+            return value;
+        } else {
+            logger.error("{} 's length is not 2", bytes);
+            return 0;
+        }
+    }
 }
--- src/test/java/com/github/anilople/javajvm/utils/ByteUtilsTest.java
+++ src/test/java/com/github/anilople/javajvm/utils/ByteUtilsTest.java
@@ -12,4 +12,16 @@ public class ByteUtilsTest {
         Assertions.assertEquals(0, ByteUtils.bytes2int(new byte[2]));
         Assertions.assertEquals(0, ByteUtils.bytes2int(new byte[4]));
     }
+
+    @Test
+    public void bytes2shortTest() {
+        byte[] zero = new byte[2];
+        Assertions.assertEquals((short) 0, ByteUtils.bytes2short(zero));
+        byte[] minusOne = { (byte) 0xFF, (byte) 0xFF };
+        Assertions.assertEquals((short) -1, ByteUtils.bytes2short(minusOne));
+        byte[] minorVersion = { (byte) 0x00, (byte) 0x00 };
+        Assertions.assertEquals((short) 0, ByteUtils.bytes2short(minorVersion));
+        byte[] majorVersion = { (byte) 0x00, (byte) 0x34 };
+        Assertions.assertEquals((short) 52, ByteUtils.bytes2short(majorVersion));
+    }
 }
