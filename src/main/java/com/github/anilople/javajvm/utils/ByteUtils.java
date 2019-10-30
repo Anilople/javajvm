eaea91488cb9eac319982d3e5be3304f76982c43
Anilople <Anilople@outlook.com>
2019-10-30 21:06:17 +0800

    add bytes2long

--- src/main/java/com/github/anilople/javajvm/utils/ByteUtils.java
+++ src/main/java/com/github/anilople/javajvm/utils/ByteUtils.java
@@ -3,6 +3,8 @@ package com.github.anilople.javajvm.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 /**
  * utils for handling byte
  * value in jvm is big end order, not small end order
@@ -10,31 +12,6 @@ import org.slf4j.LoggerFactory;
 public class ByteUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(ByteUtils.class);
-
-    /**
-     * convert 4 bytes to an int
-     * when a byte extend to an int
-     * 0xF0 -> 0xFFFFFFF0
-     * if you want 0xF0 -> 0x000000F0
-     * you must use ((int) b & 0xFF) manually
-     * @param bytes length must be 4
-     * @return
-     */
-    public static int bytes2int(byte[] bytes) {
-        if(4 == bytes.length) {
-            int value = 0xFF & ((int) bytes[0]);
-            value <<= 8;
-            value |= 0xFF & ((int) bytes[1]);
-            value <<= 8;
-            value |= 0xFF & ((int) bytes[2]);
-            value <<= 8;
-            value |= 0xFF & ((int) bytes[3]);
-            return value;
-        } else {
-            logger.error("{} 's length is not 4", bytes);
-            return 0;
-        }
-    }
 
     /**
      * convert 2 bytes to a short
@@ -46,6 +23,9 @@ public class ByteUtils {
      * @return a short
      */
     public static short bytes2short(byte[] bytes) {
+        if(2 != bytes.length) {
+            throw new RuntimeException("bytes.length must 2");
+        }
         if(2 == bytes.length) {
             int v0 = (bytes[0] & 0xFF) << 8;
             int v1 = (bytes[1] & 0xFF) ;
@@ -55,5 +35,46 @@ public class ByteUtils {
             logger.error("{} 's length is not 2", bytes);
             return 0;
         }
+    }
+
+    /**
+     * convert 4 bytes to an int
+     * when a byte extend to an int
+     * 0xF0 -> 0xFFFFFFF0
+     * if you want 0xF0 -> 0x000000F0
+     * you must use ((int) b & 0xFF) manually
+     * @param bytes length must be 4
+     * @return
+     */
+    public static int bytes2int(byte[] bytes) {
+        if(4 != bytes.length) {
+            throw new RuntimeException("bytes.length must 4");
+        }
+        int value = 0xFF & ((int) bytes[0]);
+        value <<= 8;
+        value |= 0xFF & ((int) bytes[1]);
+        value <<= 8;
+        value |= 0xFF & ((int) bytes[2]);
+        value <<= 8;
+        value |= 0xFF & ((int) bytes[3]);
+        return value;
+    }
+
+    /**
+     * convert 8 bytes to a long
+     * @param bytes length must be 8
+     * @return a long
+     */
+    public static long bytes2long(byte[] bytes) {
+        if(8 != bytes.length) {
+            throw new RuntimeException("bytes.length must 8");
+        }
+        int highValue = ByteUtils.bytes2int(Arrays.copyOfRange(bytes, 0, 4));
+        int lowValue = ByteUtils.bytes2int(Arrays.copyOfRange(bytes, 4, 8));
+
+        long value = highValue;
+        value <<= 32;
+        value |= lowValue;
+        return value;
     }
 }
--- src/test/java/com/github/anilople/javajvm/utils/ByteUtilsTest.java
+++ src/test/java/com/github/anilople/javajvm/utils/ByteUtilsTest.java
@@ -6,14 +6,6 @@ import org.junit.jupiter.api.Test;
 public class ByteUtilsTest {
 
     @Test
-    public void bytes2intTest() {
-        byte[] magicBytes = {(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE};
-        Assertions.assertEquals(0xCAFEBABE, ByteUtils.bytes2int(magicBytes));
-        Assertions.assertEquals(0, ByteUtils.bytes2int(new byte[2]));
-        Assertions.assertEquals(0, ByteUtils.bytes2int(new byte[4]));
-    }
-
-    @Test
     public void bytes2shortTest() {
         byte[] zero = new byte[2];
         Assertions.assertEquals((short) 0, ByteUtils.bytes2short(zero));
@@ -23,5 +15,27 @@ public class ByteUtilsTest {
         Assertions.assertEquals((short) 0, ByteUtils.bytes2short(minorVersion));
         byte[] majorVersion = { (byte) 0x00, (byte) 0x34 };
         Assertions.assertEquals((short) 52, ByteUtils.bytes2short(majorVersion));
+    }
+
+    @Test
+    public void bytes2intTest() {
+        byte[] magicBytes = {(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE};
+        Assertions.assertEquals(0xCAFEBABE, ByteUtils.bytes2int(magicBytes));
+        Assertions.assertThrows(RuntimeException.class, () -> ByteUtils.bytes2int(new byte[2]));
+        Assertions.assertEquals(0, ByteUtils.bytes2int(new byte[4]));
+    }
+
+    @Test
+    public void bytes2longTest() {
+        byte[] firstEightBytes = {
+                (byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE,
+                (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x34
+        };
+        Assertions.assertEquals(
+                0xCAFEBABE00000034L,
+                ByteUtils.bytes2long(firstEightBytes)
+        );
+        byte[] zero = new byte[8];
+        Assertions.assertEquals(0L, ByteUtils.bytes2long(zero));
     }
 }
