0a79f668bf565c6d8369ba3b406be71673c98966
Anilople <Anilople@outlook.com>
2019-11-02 21:19:50 +0800

    control instructions generate by python

---
+++ src/main/java/com/github/anilople/javajvm/instructions/control/ARETURN.java
@@ -0,0 +1,19 @@
+package com.github.anilople.javajvm.instructions.control;
+
+import com.github.anilople.javajvm.instructions.BytecodeReader;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.Frame;
+
+public class ARETURN implements Instruction {
+
+    @Override
+    public void FetchOperands(BytecodeReader bytecodeReader) {
+
+    }
+
+    @Override
+    public void Execute(Frame frame) {
+
+    }
+
+}
