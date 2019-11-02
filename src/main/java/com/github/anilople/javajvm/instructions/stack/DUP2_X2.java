07f8f145fa768feab915cca1fbc417a5dce5a929
Anilople <Anilople@outlook.com>
2019-11-02 20:59:34 +0800

    stack instructions

---
+++ src/main/java/com/github/anilople/javajvm/instructions/stack/DUP2_X2.java
@@ -0,0 +1,18 @@
+package com.github.anilople.javajvm.instructions.stack;
+
+import com.github.anilople.javajvm.instructions.BytecodeReader;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.Frame;
+
+public class DUP2_X2 implements Instruction {
+    
+    @Override
+    public void FetchOperands(BytecodeReader bytecodeReader) {
+
+    }
+
+    @Override
+    public void Execute(Frame frame) {
+
+    }
+}
