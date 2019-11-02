8acb4dc98c555621a10017b9f8a2cdd0325ecb6b
Anilople <Anilople@outlook.com>
2019-11-02 20:56:30 +0800

    stores instructions generate by python

---
+++ src/main/java/com/github/anilople/javajvm/instructions/stores/IASTORE.java
@@ -0,0 +1,19 @@
+package com.github.anilople.javajvm.instructions.stores;
+
+import com.github.anilople.javajvm.instructions.BytecodeReader;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.Frame;
+
+public class IASTORE implements Instruction {
+
+    @Override
+    public void FetchOperands(BytecodeReader bytecodeReader) {
+
+    }
+
+    @Override
+    public void Execute(Frame frame) {
+
+    }
+
+}
