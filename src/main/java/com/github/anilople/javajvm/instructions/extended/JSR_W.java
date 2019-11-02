2720c60ca4cb7f8fd5cac20b4c402f13b13b0444
Anilople <Anilople@outlook.com>
2019-11-02 21:21:03 +0800

    extended instructions generate by python

---
+++ src/main/java/com/github/anilople/javajvm/instructions/extended/JSR_W.java
@@ -0,0 +1,19 @@
+package com.github.anilople.javajvm.instructions.extended;
+
+import com.github.anilople.javajvm.instructions.BytecodeReader;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.Frame;
+
+public class JSR_W implements Instruction {
+
+    @Override
+    public void FetchOperands(BytecodeReader bytecodeReader) {
+
+    }
+
+    @Override
+    public void Execute(Frame frame) {
+
+    }
+
+}
