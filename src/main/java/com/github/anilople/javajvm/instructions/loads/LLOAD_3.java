7f596b4f838e4918742a38d1f0f4ba347d934b3d
Anilople <Anilople@outlook.com>
2019-11-02 20:52:35 +0800

    loads instructions generate by python

---
+++ src/main/java/com/github/anilople/javajvm/instructions/loads/LLOAD_3.java
@@ -0,0 +1,19 @@
+package com.github.anilople.javajvm.instructions.loads;
+
+import com.github.anilople.javajvm.instructions.BytecodeReader;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.Frame;
+
+public class LLOAD_3 implements Instruction {
+
+    @Override
+    public void FetchOperands(BytecodeReader bytecodeReader) {
+
+    }
+
+    @Override
+    public void Execute(Frame frame) {
+        
+    }
+
+}
