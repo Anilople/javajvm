30ca3829a05c1e6d1bd9466c83973ae7bee6f8b9
Anilople <Anilople@outlook.com>
2019-11-02 21:16:48 +0800

    comparisons instructions generate by python

---
+++ src/main/java/com/github/anilople/javajvm/instructions/comparisons/IF_ACMPNE.java
@@ -0,0 +1,19 @@
+package com.github.anilople.javajvm.instructions.comparisons;
+
+import com.github.anilople.javajvm.instructions.BytecodeReader;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.Frame;
+
+public class IF_ACMPNE implements Instruction {
+
+    @Override
+    public void FetchOperands(BytecodeReader bytecodeReader) {
+
+    }
+
+    @Override
+    public void Execute(Frame frame) {
+
+    }
+
+}
