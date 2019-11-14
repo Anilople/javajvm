f19d284ef933cec4d7a07a36a0c8236c6badc14f
Anilople <Anilople@outlook.com>
2019-11-14 21:52:41 +0800

    change object reference, add run-time fields space

--- src/main/java/com/github/anilople/javajvm/runtimedataarea/reference/ObjectReference.java
+++ src/main/java/com/github/anilople/javajvm/runtimedataarea/reference/ObjectReference.java
@@ -1,20 +1,23 @@
 package com.github.anilople.javajvm.runtimedataarea.reference;
 
 import com.github.anilople.javajvm.heap.JvmClass;
+import com.github.anilople.javajvm.runtimedataarea.LocalVariables;
 import com.github.anilople.javajvm.runtimedataarea.Reference;
 
-public class ObjectReference implements Reference {
+public class ObjectReference extends LocalVariables implements Reference {
 
     private JvmClass jvmClass;
 
-
-
-    private ObjectReference() {
-
-    }
-
     public ObjectReference(JvmClass jvmClass) {
+        // an Object in run-time exist own fields (non static fields)
+        super(jvmClass.getNonStaticFieldsSize());
         this.jvmClass = jvmClass;
     }
 
+    @Override
+    public String toString() {
+        return "ObjectReference{" +
+                super.toString() +
+                '}';
+    }
 }
