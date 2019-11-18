e5fd1e54422e127fe9a252ed6d0f18b0e1ab7b11
Anilople <Anilople@outlook.com>
2019-11-18 23:32:41 +0800

    descriptor utils, and test template

--- src/main/java/com/github/anilople/javajvm/utils/DescriptorUtils.java
+++ src/main/java/com/github/anilople/javajvm/utils/DescriptorUtils.java
@@ -38,9 +38,31 @@ import com.github.anilople.javajvm.constants.Descriptors;
 public class DescriptorUtils {
 
     /**
+     * FieldDescriptor:
+     *      FieldType
+     * @param descriptor
+     * @return
+     */
+    public static boolean isFieldDescriptor(String descriptor) {
+        return isFieldType(descriptor);
+    }
+
+    /**
+     * FieldType:
+     *      BaseType
+     *      ObjectType
+     *      ArrayType
+     * @param descriptor
+     * @return
+     */
+    public static boolean isFieldType(String descriptor) {
+        return isBaseType(descriptor) || isObjectType(descriptor) || isArrayType(descriptor);
+    }
+
+    /**
      * BaseType:
-     *  (one of)
-     *  B C D F I J S Z
+     *      (one of)
+     *      B C D F I J S Z
      * @param descriptor
      * @return
      */
@@ -57,12 +79,98 @@ public class DescriptorUtils {
 
     /**
      * ObjectType:
-     *  L ClassName ;
+     *      L ClassName ;
      * @param descriptor
      * @return
      */
     public static boolean isObjectType(String descriptor) {
-        return descriptor.startsWith("L") && descriptor.endsWith(";");
+        return descriptor.startsWith("L")
+                && descriptor.endsWith(";")
+                && isClassName(descriptor.substring(1, descriptor.length() - 1));
+    }
+
+    /**
+     * judge string is a valid class name or not
+     * @param className
+     * @return
+     */
+    public static boolean isClassName(String className) {
+        return true;
+    }
+
+
+    /**
+     * ArrayType:
+     *      [ ComponentType
+     * @param descriptor
+     * @return
+     */
+    public static boolean isArrayType(String descriptor) {
+        return descriptor.startsWith("[")
+                && isComponentType(descriptor.substring(1));
+    }
+
+    /**
+     * ComponentType:
+     *      FieldType
+     * @param componentType
+     * @return
+     */
+    public static boolean isComponentType(String componentType) {
+        return isFieldType(componentType);
+    }
+
+
+    /**
+     * get the content in first Parentheses pair
+     * suppose first char is '('
+     * @param descriptor
+     * @return
+     */
+    private static String getInFirstParentheses(String descriptor) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 1; i < descriptor.length() && (')' != descriptor.charAt(i)); i++) {
+            stringBuilder.append(descriptor.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * MethodDescriptor:
+     *      ( {ParameterDescriptor} ) ReturnDescriptor
+     * @param descriptor
+     * @return
+     */
+    public static boolean isMethodDescriptor(String descriptor) {
+        String parameterDescriptor =  getInFirstParentheses(descriptor);
+        String returnDescriptor = descriptor.substring(descriptor.lastIndexOf(')') + 1);
+        return (parameterDescriptor.length() <= 0 || isParameterDescriptor(parameterDescriptor))
+                && isReturnDescriptor(returnDescriptor);
+    }
+
+    /**
+     * ParameterDescriptor:
+     *      FieldType
+     * @param parameterDescriptor
+     * @return
+     */
+    public static boolean isParameterDescriptor(String parameterDescriptor) {
+        return isFieldType(parameterDescriptor);
+    }
+
+    /**
+     * ReturnDescriptor:
+     *      FieldType
+     *      VoidDescriptor
+     * @param returnDescriptor
+     * @return
+     */
+    public static boolean isReturnDescriptor(String returnDescriptor) {
+        return isFieldType(returnDescriptor) || isVoidDescriptor(returnDescriptor);
+    }
+
+    public static boolean isVoidDescriptor(String voidDescriptor) {
+        return voidDescriptor.equals("V");
     }
 
     /**
@@ -72,15 +180,6 @@ public class DescriptorUtils {
      */
     public static String getClassName(String descriptor) {
         return descriptor.substring(1, descriptor.length() - 1);
-    }
-
-    /**
-     * to do
-     * @param descriptor
-     * @return
-     */
-    public static boolean isArrayType(String descriptor) {
-        return descriptor.startsWith("[");
     }
 
 }
---
+++ src/test/java/com/github/anilople/javajvm/utils/DescriptorUtilsTest.java
@@ -0,0 +1,57 @@
+package com.github.anilople.javajvm.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DescriptorUtilsTest {
+
+    @Test
+    void isFieldDescriptor() {
+    }
+
+    @Test
+    void isFieldType() {
+    }
+
+    @Test
+    void isBaseType() {
+    }
+
+    @Test
+    void isObjectType() {
+    }
+
+    @Test
+    void isClassName() {
+    }
+
+    @Test
+    void isArrayType() {
+    }
+
+    @Test
+    void isComponentType() {
+    }
+
+    @Test
+    void isMethodDescriptor() {
+    }
+
+    @Test
+    void isParameterDescriptor() {
+    }
+
+    @Test
+    void isReturnDescriptor() {
+    }
+
+    @Test
+    void isVoidDescriptor() {
+    }
+
+    @Test
+    void getClassName() {
+    }
+}
