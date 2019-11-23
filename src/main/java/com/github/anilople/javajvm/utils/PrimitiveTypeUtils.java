c706667e38f378e2be3f48d3ba94062ed64a165c
Anilople <Anilople@outlook.com>
2019-11-23 17:53:15 +0800

    add some type convert method

--- src/main/java/com/github/anilople/javajvm/utils/PrimitiveTypeUtils.java
+++ src/main/java/com/github/anilople/javajvm/utils/PrimitiveTypeUtils.java
@@ -2,15 +2,46 @@ package com.github.anilople.javajvm.utils;
 
 public class PrimitiveTypeUtils {
 
+    /**
+     * unsigned extend
+     * @param b
+     * @return
+     */
     public static int intFormUnsignedByte(byte b) {
         return ((int) b) & 0xFF;
     }
 
+    /**
+     * signed extend
+     */
+    public static int intFormSignedByte(byte b) {
+        return (int) b;
+    }
+
+    /**
+     * unsigned extend
+     * @param b
+     * @return
+     */
     public static short shortFormUnsignedByte(byte b) {
         return (short) intFormUnsignedByte(b);
     }
 
+    /**
+     * unsigned extend
+     * @param s
+     * @return
+     */
     public static int intFormUnsignedShort(short s) {
         return 0x0000FFFF & s;
+    }
+
+    /**
+     * signed extend
+     * @param s
+     * @return
+     */
+    public static int intFormSignedShort(short s) {
+        return (int) s;
     }
 }
