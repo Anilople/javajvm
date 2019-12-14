66713fda2dfd08f19ccc67a40b326203472f8bc7
anilople <anilople@outlook.com>
2019-12-14 10:48:00 +0800

    delete import sun.security.krb5.internal.crypto.Des which is not compatible

--- src/main/java/com/github/anilople/javajvm/instructions/references/GETSTATIC.java
+++ src/main/java/com/github/anilople/javajvm/instructions/references/GETSTATIC.java
@@ -12,7 +12,6 @@ import com.github.anilople.javajvm.utils.DescriptorUtils;
 import com.github.anilople.javajvm.utils.PrimitiveTypeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.security.krb5.internal.crypto.Des;
 
 /**
  * Operation
