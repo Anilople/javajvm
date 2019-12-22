a88252e0a5dc9be0b7a79ce2ba00c625f1cbf404
Anilople <Anilople@outlook.com>
2019-12-22 09:29:04 +0800

    StringTest

---
+++ src/test/java/com/github/anilople/javajvm/testcode/StringTest.java
@@ -0,0 +1,21 @@
+package com.github.anilople.javajvm.testcode;
+
+import com.github.anilople.javajvm.JavaJvmApplication;
+import com.github.anilople.javajvm.helper.CommandConfig;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    public void mainTest() {
+        String className = this.getClass().getName();
+        System.out.println(className);
+        JavaJvmApplication.main(CommandConfig.getArgs(className));
+    }
+
+    public static void main(String[] args) {
+        String a = "test34234";
+        String b = a;
+        String c = a + b;
+    }
+}
