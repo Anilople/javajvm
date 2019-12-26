2d3bd3db7e297d34afd4626e1fcf8718b369f21d
Anilople <Anilople@outlook.com>
2019-12-26 22:20:06 +0800

    refactoring test

--- src/test/java/com/github/anilople/javajvm/instructions/references/ANEWARRAYTest.java
+++ src/test/java/com/github/anilople/javajvm/instructions/references/ANEWARRAYTest.java
@@ -1,14 +1,15 @@
 package com.github.anilople.javajvm.instructions.references;
 
-import com.github.anilople.javajvm.heap.JvmClassLoader;
-import com.github.anilople.javajvm.heap.JvmMethod;
+import com.github.anilople.javajvm.helper.HighOrderFunctions;
 import com.github.anilople.javajvm.helper.JvmThreadFactory;
 import com.github.anilople.javajvm.helper.JvmThreadRunner;
+import com.github.anilople.javajvm.runtimedataarea.JvmThread;
 import com.github.anilople.javajvm.runtimedataarea.reference.ObjectArrayReference;
 import com.github.anilople.javajvm.runtimedataarea.reference.ObjectReference;
-import com.github.anilople.javajvm.utils.JvmMethodUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
 
 class ANEWARRAYTest {
 
@@ -16,25 +17,23 @@ class ANEWARRAYTest {
         ANEWARRAYTest[] anewarrayTests = new ANEWARRAYTest[6];
     }
 
+    private final Consumer<JvmThread> afterInstructionExecutionListener = jvmThread -> {
+        // in the ANEWARRAYTest's "main" method, so after Instruction "ANEWARRAY",
+        ObjectArrayReference objectArrayReference = (ObjectArrayReference) jvmThread.currentFrame().getOperandStacks().popReference();
+        // the top Object on the operand stack must be the array of "ANEWARRAYTest"
+        ObjectReference typeReference = (ObjectReference) objectArrayReference.getTypeReference();
+        Assertions.assertTrue(typeReference.getJvmClass().isSameName(ANEWARRAYTest.class));
+        // remember that push it back
+        jvmThread.currentFrame().getOperandStacks().pushReference(objectArrayReference);
+    };
+
     @Test
     void execute() {
-        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(JvmThreadFactory.makeSimpleInstance(ANEWARRAYTest.class));
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(JvmThreadFactory.makeSimpleInstance(this.getClass()));
         
         jvmThreadRunner.addAfterInstructionExecutionListener(
                 ANEWARRAY.class,
-                jvmThread -> {
-                    JvmClassLoader jvmClassLoader = jvmThread.currentFrame().getJvmMethod().getJvmClass().getLoader();
-                    JvmMethod mainMethod = JvmMethodUtils.getMainMethod(jvmClassLoader, ANEWARRAYTest.class);
-                    if(jvmThread.currentFrame().getJvmMethod().equals(mainMethod)) {
-                        // in the ANEWARRAYTest's "main" method, so after Instruction "ANEWARRAY",
-                        ObjectArrayReference objectArrayReference = (ObjectArrayReference) jvmThread.currentFrame().getOperandStacks().popReference();
-                        // the top Object on the operand stack must be the array of "ANEWARRAYTest"
-                        ObjectReference typeReference = (ObjectReference) objectArrayReference.getTypeReference();
-                        Assertions.assertTrue(typeReference.getJvmClass().isSameName(ANEWARRAYTest.class));
-                        // remember that push it back
-                        jvmThread.currentFrame().getOperandStacks().pushReference(objectArrayReference);
-                    }
-                }
+                HighOrderFunctions.toInMainTrigger(this.getClass(), afterInstructionExecutionListener)
         );
 
         jvmThreadRunner.run();
--- src/test/java/com/github/anilople/javajvm/instructions/references/NEWTest.java
+++ src/test/java/com/github/anilople/javajvm/instructions/references/NEWTest.java
@@ -1,19 +1,29 @@
 package com.github.anilople.javajvm.instructions.references;
 
-import com.github.anilople.javajvm.heap.JvmClassLoader;
-import com.github.anilople.javajvm.heap.JvmMethod;
+import com.github.anilople.javajvm.helper.HighOrderFunctions;
 import com.github.anilople.javajvm.helper.JvmThreadFactory;
 import com.github.anilople.javajvm.helper.JvmThreadRunner;
+import com.github.anilople.javajvm.runtimedataarea.JvmThread;
 import com.github.anilople.javajvm.runtimedataarea.reference.ObjectReference;
-import com.github.anilople.javajvm.utils.JvmMethodUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
 
 class NEWTest {
 
     public static void main(String[] args) {
         NEWTest newTest = new NEWTest();
     }
+
+    private final Consumer<JvmThread> afterInstructionExecutionListener = jvmThread -> {
+        // in the NEWTest's "main" method, so after Instruction "NEW",
+        ObjectReference objectReference = (ObjectReference) jvmThread.currentFrame().getOperandStacks().popReference();
+        // the top Object on the operand stack must be class "NEWTest"
+        Assertions.assertTrue(objectReference.getJvmClass().isSameName(NEWTest.class));
+        // remember that push it back
+        jvmThread.currentFrame().getOperandStacks().pushReference(objectReference);
+    };
 
     @Test
     void execute() {
@@ -22,19 +32,7 @@ class NEWTest {
 
         jvmThreadRunner.addAfterInstructionExecutionListener(
                 NEW.class,
-                jvmThread -> {
-                    // maybe a target object on the top of operand stack now
-                    JvmClassLoader jvmClassLoader = jvmThread.currentFrame().getJvmMethod().getJvmClass().getLoader();
-                    JvmMethod mainMethod = JvmMethodUtils.getMainMethod(jvmClassLoader, NEWTest.class);
-                    if(jvmThread.currentFrame().getJvmMethod().equals(mainMethod)) {
-                        // in the NEWTest's "main" method, so after Instruction "NEW",
-                        ObjectReference objectReference = (ObjectReference) jvmThread.currentFrame().getOperandStacks().popReference();
-                        // the top Object on the operand stack must be class "NEWTest"
-                        Assertions.assertTrue(objectReference.getJvmClass().isSameName(NEWTest.class));
-                        // remember that push it back
-                        jvmThread.currentFrame().getOperandStacks().pushReference(objectReference);
-                    }
-                }
+                HighOrderFunctions.toInMainTrigger(this.getClass(), afterInstructionExecutionListener)
         );
 
         jvmThreadRunner.run();
