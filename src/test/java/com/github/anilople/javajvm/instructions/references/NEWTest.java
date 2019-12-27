ca9123f5ac8725b099eb049a93e656897b432c59
Anilople <Anilople@outlook.com>
2019-12-27 23:09:48 +0800

    Due to the instruction invokedynamic, move the lambda to method's body

--- src/test/java/com/github/anilople/javajvm/instructions/references/NEWTest.java
+++ src/test/java/com/github/anilople/javajvm/instructions/references/NEWTest.java
@@ -17,19 +17,19 @@ class NEWTest {
         NEWTest newTest = new NEWTest();
     }
 
-    private final Consumer<JvmThread> afterInstructionExecutionListener = jvmThread -> {
-        // in the NEWTest's "main" method, so after Instruction "NEW",
-        ObjectReference objectReference = (ObjectReference) jvmThread.currentFrame().getOperandStacks().popReference();
-        // the top Object on the operand stack must be class "NEWTest"
-        assertTrue(objectReference.getJvmClass().isSameName(NEWTest.class));
-        // remember that push it back
-        jvmThread.currentFrame().getOperandStacks().pushReference(objectReference);
-    };
-
     @Test
     void execute() {
 
-        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(JvmThreadFactory.makeSimpleInstance(NEWTest.class));
+        final Consumer<JvmThread> afterInstructionExecutionListener = jvmThread -> {
+            // in the NEWTest's "main" method, so after Instruction "NEW",
+            ObjectReference objectReference = (ObjectReference) jvmThread.currentFrame().getOperandStacks().popReference();
+            // the top Object on the operand stack must be class "NEWTest"
+            assertTrue(objectReference.getJvmClass().isSameName(NEWTest.class));
+            // remember that push it back
+            jvmThread.currentFrame().getOperandStacks().pushReference(objectReference);
+        };
+
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(JvmThreadFactory.makeSimpleInstance(this.getClass()));
 
         jvmThreadRunner.addAfterInstructionExecutionListener(
                 NEW.class,
