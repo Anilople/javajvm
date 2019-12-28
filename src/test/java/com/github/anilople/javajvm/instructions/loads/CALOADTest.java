6884ceee93ce9705b81fcfaa29e1bbcc05be102b
Anilople <Anilople@outlook.com>
2019-12-28 17:15:03 +0800

    CALOADTest, CASTORETest

--- src/test/java/com/github/anilople/javajvm/instructions/loads/CALOADTest.java
+++ src/test/java/com/github/anilople/javajvm/instructions/loads/CALOADTest.java
@@ -1,16 +1,18 @@
 package com.github.anilople.javajvm.instructions.loads;
 
-import com.github.anilople.javajvm.JavaJvmApplication;
-import com.github.anilople.javajvm.helper.CommandConfig;
+import com.github.anilople.javajvm.helper.HighOrderFunctions;
+import com.github.anilople.javajvm.helper.JvmThreadFactory;
+import com.github.anilople.javajvm.helper.JvmThreadRunner;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.JvmThread;
 import org.junit.jupiter.api.Test;
 
-public class CALOADTest {
+import java.util.function.BiConsumer;
 
-    @Test
-    public void mainTest() {
-        String className = this.getClass().getName();
-        JavaJvmApplication.main(CommandConfig.getArgs(className));
-    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CALOADTest {
 
     public static void main(String[] args) {
         char[] chars = new char[4];
@@ -18,4 +20,36 @@ public class CALOADTest {
         char temp = chars[3];
     }
 
+    @Test
+    void execute() {
+
+        final BiConsumer<Instruction, JvmThread> beforeListener = (instruction, jvmThread) -> {
+            assertTrue(instruction instanceof CALOAD);
+            int index = jvmThread.currentFrame().getOperandStacks().popIntValue();
+            // check the array index
+            assertEquals(3, index);
+            jvmThread.currentFrame().getOperandStacks().pushIntValue(index);
+        };
+
+        final BiConsumer<Instruction, JvmThread> afterListener = (instruction, jvmThread) -> {
+            char fromOperandStack = jvmThread.currentFrame().getOperandStacks().popCharValue();
+            assertEquals('a', fromOperandStack);
+            jvmThread.currentFrame().getOperandStacks().pushCharValue(fromOperandStack);
+        };
+
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(JvmThreadFactory.makeSimpleInstance(this.getClass()));
+
+        jvmThreadRunner.addBeforeInstructionExecutionListener(
+                CALOAD.class,
+                HighOrderFunctions.toInMainTrigger(this.getClass(), beforeListener)
+        );
+
+        jvmThreadRunner.addAfterInstructionExecutionListener(
+                CALOAD.class,
+                HighOrderFunctions.toInMainTrigger(this.getClass(), afterListener)
+        );
+
+        jvmThreadRunner.run();
+        
+    }
 }
--- src/test/java/com/github/anilople/javajvm/instructions/stores/CASTORETest.java
+++ src/test/java/com/github/anilople/javajvm/instructions/stores/CASTORETest.java
@@ -1,21 +1,65 @@
 package com.github.anilople.javajvm.instructions.stores;
 
-import com.github.anilople.javajvm.JavaJvmApplication;
-import com.github.anilople.javajvm.helper.CommandConfig;
+import com.github.anilople.javajvm.helper.HighOrderFunctions;
+import com.github.anilople.javajvm.helper.JvmThreadFactory;
+import com.github.anilople.javajvm.helper.JvmThreadRunner;
+import com.github.anilople.javajvm.instructions.Instruction;
+import com.github.anilople.javajvm.runtimedataarea.JvmThread;
+import com.github.anilople.javajvm.runtimedataarea.reference.BaseTypeArrayReference;
 import org.junit.jupiter.api.Test;
 
-public class CASTORETest {
+import java.util.function.BiConsumer;
 
-    @Test
-    public void mainTest() {
-        String className = this.getClass().getName();
-        JavaJvmApplication.main(CommandConfig.getArgs(className));
-    }
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CASTORETest {
 
     public static void main(String[] args) {
         char[] chars = new char[20];
         int len = chars.length;
-        chars[0] = '6';
         chars[chars.length - 1] = 'k';
+    }
+
+    private BaseTypeArrayReference baseTypeArrayReference = null;
+
+    @Test
+    void execute() {
+
+        final BiConsumer<Instruction, JvmThread> beforeListener = (instruction, jvmThread) -> {
+            assertTrue(instruction instanceof CASTORE);
+            char value = jvmThread.currentFrame().getOperandStacks().popCharValue();
+            int index = jvmThread.currentFrame().getOperandStacks().popIntValue();
+            this.baseTypeArrayReference = (BaseTypeArrayReference) jvmThread.currentFrame().getOperandStacks().popReference();
+            // check the array value
+            assertEquals('k', value);
+            // check the array index
+            assertEquals(19, index);
+
+            // push them back
+            jvmThread.currentFrame().getOperandStacks().pushReference(this.baseTypeArrayReference);
+            jvmThread.currentFrame().getOperandStacks().pushIntValue(index);
+            jvmThread.currentFrame().getOperandStacks().pushIntValue(value);
+        };
+
+        final BiConsumer<Instruction, JvmThread> afterListener = (instruction, jvmThread) -> {
+            assertNotNull(baseTypeArrayReference);
+            assertEquals('k', baseTypeArrayReference.getCharValue(19));
+        };
+
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(JvmThreadFactory.makeSimpleInstance(this.getClass()));
+
+        jvmThreadRunner.addBeforeInstructionExecutionListener(
+                CASTORE.class,
+                HighOrderFunctions.toInMainTrigger(this.getClass(), beforeListener)
+        );
+
+        jvmThreadRunner.addAfterInstructionExecutionListener(
+                CASTORE.class,
+                HighOrderFunctions.toInMainTrigger(this.getClass(), afterListener)
+        );
+
+        jvmThreadRunner.run();
+        
     }
 }
