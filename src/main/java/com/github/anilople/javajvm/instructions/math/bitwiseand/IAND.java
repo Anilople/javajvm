2e2cb37c4a0454b79f8f5ab30eaefcd39c3c575c
Anilople <Anilople@outlook.com>
2020-01-12 17:42:51 +0800

    Add: IAND, LAND

--- src/main/java/com/github/anilople/javajvm/instructions/math/bitwiseand/IAND.java
+++ src/main/java/com/github/anilople/javajvm/instructions/math/bitwiseand/IAND.java
@@ -4,6 +4,19 @@ import com.github.anilople.javajvm.instructions.BytecodeReader;
 import com.github.anilople.javajvm.instructions.Instruction;
 import com.github.anilople.javajvm.runtimedataarea.Frame;
 
+/**
+ * Operation
+ * Boolean AND int
+ *
+ * Operand ..., value1, value2 →
+ * Stack ..., result
+ *
+ * Description
+ * Both value1 and value2 must be of type int . They are popped
+ * from the operand stack. An int result is calculated by taking the
+ * bitwise AND (conjunction) of value1 and value2. The result is
+ * pushed onto the operand stack.
+ */
 public class IAND implements Instruction {
 
     @Override
@@ -13,9 +26,15 @@ public class IAND implements Instruction {
 
     @Override
     public void execute(Frame frame) {
-        throw new RuntimeException("Now cannot support " + this.getClass());
-//        int nextPc = frame.getNextPc() + this.size();
-//        frame.setNextPc(nextPc);
+        final int value2 = frame.getOperandStacks().popIntValue();
+        final int value1 = frame.getOperandStacks().popIntValue();
+
+        final int result = value1 & value2;
+
+        frame.getOperandStacks().pushIntValue(result);
+
+        int nextPc = frame.getNextPc() + this.size();
+        frame.setNextPc(nextPc);
     }
 
     @Override
