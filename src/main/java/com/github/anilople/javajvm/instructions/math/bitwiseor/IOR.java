24caf6e8ac7e2974e6a1ff97f7ee51af38fc379d
Anilople <Anilople@outlook.com>
2020-01-29 17:09:18 +0800

    Add: LOR, LXOR, IOR, IXOR

--- src/main/java/com/github/anilople/javajvm/instructions/math/bitwiseor/IOR.java
+++ src/main/java/com/github/anilople/javajvm/instructions/math/bitwiseor/IOR.java
@@ -4,6 +4,19 @@ import com.github.anilople.javajvm.instructions.BytecodeReader;
 import com.github.anilople.javajvm.instructions.Instruction;
 import com.github.anilople.javajvm.runtimedataarea.Frame;
 
+/**
+ * Operation
+ * Boolean OR int
+ *
+ * Operand ..., value1, value2 →
+ * Stack ..., result
+ *
+ * Description
+ * Both value1 and value2 must be of type int . They are popped from
+ * the operand stack. An int result is calculated by taking the bitwise
+ * inclusive OR of value1 and value2. The result is pushed onto the
+ * operand stack.
+ */
 public class IOR implements Instruction {
 
     @Override
@@ -13,9 +26,12 @@ public class IOR implements Instruction {
 
     @Override
     public void execute(Frame frame) {
-        throw new RuntimeException("Now cannot support " + this.getClass());
-//        int nextPc = frame.getNextPc() + this.size();
-//        frame.setNextPc(nextPc);
+        int value2 = frame.getOperandStacks().popIntValue();
+        int value1 = frame.getOperandStacks().popIntValue();
+        int result = value1 | value2;
+        frame.getOperandStacks().pushIntValue(result);
+        int nextPc = frame.getNextPc() + this.size();
+        frame.setNextPc(nextPc);
     }
 
     @Override
