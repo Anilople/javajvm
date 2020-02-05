01faa458a5da8eadfdae4a070eab52f4bde5e7f9
Anilople <Anilople@outlook.com>
2020-02-05 23:25:28 +0800

    Add: java.util.Map test. Cause some problem when get constant from a wrong constant pool

---
+++ src/test/java/com/github/anilople/javajvm/testcode/container/MapTest.java
@@ -0,0 +1,121 @@
+package com.github.anilople.javajvm.testcode.container;
+
+import com.github.anilople.javajvm.helper.JvmThreadFactory;
+import com.github.anilople.javajvm.helper.JvmThreadRunner;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * test container map in self jvm
+ * @see java.util.Map
+ */
+public class MapTest {
+
+    public static void main(String[] args) {
+        hashMapPut();
+        reverseKeyValue();
+    }
+
+    private static void hashMapPut() {
+        // name -> age
+        Map<String, Integer> name2age = new HashMap<>();
+        name2age.put("C", 3);
+        name2age.put("A", 1);
+        name2age.put("B", 2);
+        name2age.put("D", 4);
+        name2age.put("DD", 4);
+        System.out.println(name2age);
+    }
+
+    @Test
+    void hashMapPutTest() {
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(
+                JvmThreadFactory.createFromStaticMethod(
+                        this.getClass(),
+                        "hashMapPut",
+                        "()V"
+                )
+        );
+
+        jvmThreadRunner.run();
+    }
+
+    private static void treeMapPut() {
+        Map<String, Integer> name2age = new TreeMap<>();
+        name2age.put("C", 3);
+        name2age.put("A", 1);
+        name2age.put("DD", 4);
+        System.out.println(name2age);
+    }
+
+    @Test
+    void treeMapPutTest() {
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(
+                JvmThreadFactory.createFromStaticMethod(
+                        this.getClass(),
+                        "treeMapPut",
+                        "()V"
+                )
+        );
+
+        jvmThreadRunner.run();
+    }
+
+    private static void hashMapToStringImplicitly() {
+        // name -> age
+        Map<String, Integer> name2age = new HashMap<>();
+        name2age.put("A", 1);
+        System.out.println("before reverse: " + name2age.toString());
+    }
+
+    @Test
+    void hashMapToStringImplicitlyTest() {
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(
+                JvmThreadFactory.createFromStaticMethod(
+                        this.getClass(),
+                        "hashMapToStringImplicitly",
+                        "()V"
+                )
+        );
+
+        jvmThreadRunner.run();
+    }
+
+    private static void reverseKeyValue() {
+        // name -> age
+        Map<String, Integer> name2age = new HashMap<>();
+        name2age.put("C", 3);
+        name2age.put("A", 1);
+        name2age.put("B", 2);
+        name2age.put("D", 4);
+        name2age.put("DD", 4);
+        System.out.println("before reverse: " + name2age.toString());
+
+        // age -> name
+        // will lose some record
+        Map<Integer, String> age2name = new TreeMap<>();
+        for(Map.Entry<String, Integer> entry : name2age.entrySet()) {
+            String name = entry.getKey();
+            Integer age = entry.getValue();
+            age2name.put(age, name);
+        }
+
+        System.out.println("after reverse: " + age2name.toString());
+    }
+
+    @Test
+    void reverseKeyValueTest() {
+        JvmThreadRunner jvmThreadRunner = new JvmThreadRunner(
+                JvmThreadFactory.createFromStaticMethod(
+                        this.getClass(),
+                        "reverseKeyValue",
+                        "()V"
+                )
+        );
+
+        jvmThreadRunner.run();
+    }
+}
